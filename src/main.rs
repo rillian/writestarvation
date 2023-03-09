69273091a8c64ffbcacf31d2e7114494930a4d8f
Ralph Giles <rgiles@brave.com>
2023-03-09 09:31:01 -0800

    Log thread id along with wait time.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::sync::{Arc, RwLock};
+use std::thread;
 use std::time::{Duration, Instant};
 
 fn reader(lock: Arc<RwLock<()>>) {
     // loop forever, holding a read lock for most of the time
+    let id = thread::current().id();
     loop {
         let delay = Duration::from_millis(333);
         let start = Instant::now();
         let _guard = lock.read().expect("Poisoned read lock!");
         let read_delay = start.elapsed();
-        std::thread::sleep(delay);
-        println!("Reader waited {} ms for a read lock, held it {} ms",
-            read_delay.as_millis(), delay.as_millis());
+        thread::sleep(delay);
+        println!("Reader waited {} ms for a read lock, held it {} ms {:?}",
+            read_delay.as_millis(), delay.as_millis(), id);
     }
-
 }
 
 fn main() {
@@ -20,6 +21,6 @@ fn main() {
 
     let lock = Arc::new(RwLock::new(()));
     let rlock = lock.clone();
-    let r = std::thread::spawn(move || reader(rlock));
+    let r = thread::spawn(move || reader(rlock));
     r.join().unwrap();
 }
