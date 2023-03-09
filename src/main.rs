c40deb9afd6505e7f0d86209a8cf071f4728ebc3
Ralph Giles <rgiles@brave.com>
2023-03-09 10:08:06 -0800

    Periodically request a write lock.
    The write lock isn't held over the delay. We just want to measure
    how long the writer needs to wait under reader contention. If
    writers are prioritized it shouldn't be longer than the maximum
    read-lock duration. If writers are starved it could be indefinite.

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,60 @@ fn reader(lock: Arc<RwLock<()>>) {
     loop {
         let delay = Duration::from_millis(rng.sample(dist));
         let start = Instant::now();
-        let _guard = lock.read().expect("Poisoned read lock!");
+        let _guard = lock.read().expect("Poisoned lock!");
         let read_delay = start.elapsed();
         thread::sleep(delay);
-        println!("Reader waited {} μs for a read lock, held it {} ms {:?}",
+        println!("Reader waited {} μs for a lock, held it {} ms {:?}",
             read_delay.as_micros(), delay.as_millis(), id);
     }
 }
 
+fn writer(lock: Arc<RwLock<()>>) {
+    // loop forever, asking for the write lock periodically
+    let id = thread::current().id();
+    let mut rng = rand::thread_rng();
+    let dist = rand::distributions::Uniform::new(500, 5000);
+    loop {
+        let delay = Duration::from_millis(rng.sample(dist));
+        let start = Instant::now();
+        {
+            let _guard = lock.write().expect("Poisoned lock!");
+            let write_delay = start.elapsed();
+            println!("Writer waited {} μs for a lock {:?}",
+                write_delay.as_micros(), id);
+            // lock is dropped before sleeping
+        }
+        thread::sleep(delay);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rust RwLock contetion test");
 
     // Create a lock
     let lock = Arc::new(RwLock::new(()));
-    // Pass it to a number of read threads
+
+    // How many threads do we want?
     let count = match thread::available_parallelism() {
         Ok(value) => value.get(),
         Err(_) => 4,
     };
-    let readers: Vec<thread::JoinHandle<_>> = (0..count).into_iter().map(|_| {
-        let rlock = lock.clone();
-        thread::spawn(move || reader(rlock))
-    }).collect();
+    let mut threads = Vec::with_capacity(count + 1);
+    println!("spawning {} threads...", threads.capacity());
 
-    // Wait for threads to return
-    for r in readers.into_iter() {
-        r.join().unwrap();
+    // Pass the lock to a number of read threads
+    for _ in 0..count {
+        let rlock = lock.clone();
+        let r = thread::spawn(move || reader(rlock));
+        threads.push(r);
+    }
+
+    // Pass the lock to a write thread
+    let w = thread::spawn(move || writer(lock));
+    threads.push(w);
+
+    // Wait for all threads to return
+    for t in threads.into_iter() {
+        t.join().unwrap();
     }
 }
